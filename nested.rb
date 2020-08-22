b7c28c4f0593b992674935acb32084a885269e7f
Sierra Wallace <sskwallace@gmail.com>
2020-08-22 23:20:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -70,7 +70,7 @@ def adding_matz
 	programmer_hash = 
  		{
         :grace_hopper => {
-          :known_for => "COBOL",
+          :known_for => "COBOL"
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
@@ -82,7 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     programmer_hash = [:yukihiro_matsumoto] - {:known_for => "Ruby", :languages => ["LISP", "C"]}
+     programmer_hash = [:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
      programmer_hash
     
 end
